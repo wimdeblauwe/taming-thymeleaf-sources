9e1f570bd0a9df197c2022fdcd19b1a8ad22f57d
Wim Deblauwe <wim.deblauwe@gmail.com>
2020-10-18 13:48:29 +0200

    Sources chapter 2

---
+++ chapter02/src/test/java/com/tamingthymeleaf/application/TamingThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamingthymeleaf.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamingThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
