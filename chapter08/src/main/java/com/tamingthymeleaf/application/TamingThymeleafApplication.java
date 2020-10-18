e3f834c4edc286312a5d72520718d1449fe6d94e
Wim Deblauwe <wim.deblauwe@gmail.com>
2020-10-18 14:16:23 +0200

    Sources chapter 8

---
+++ chapter08/src/main/java/com/tamingthymeleaf/application/TamingThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.tamingthymeleaf.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamingThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamingThymeleafApplication.class, args);
+	}
+
+}
