c1e2faec8730c511eb697382f8fc4d8b576ea322
Wim Deblauwe <wim.deblauwe@gmail.com>
2021-10-16 10:26:11 +0200

    Ensure smoother page refreshes
    - Add x-cloak to avoid popups flashing on page reload
    - Allow the browser to cache img and css files by ignoring them in WebSecurityConfiguration

--- chapter16/src/main/java/com/tamingthymeleaf/application/infrastructure/security/WebSecurityConfiguration.java
+++ chapter16/src/main/java/com/tamingthymeleaf/application/infrastructure/security/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,6 +29,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .passwordEncoder(passwordEncoder); //<.>
     }
     // end::configure-users[]
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+           .mvcMatchers("/img/**")
+           .mvcMatchers("/css/**");
+    }
 
     // tag::configure-http[]
     @Override
