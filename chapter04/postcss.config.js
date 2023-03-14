6809d5c09240db80f1ced9d2b59872ab43572a7d
Wim Deblauwe <wim.deblauwe@gmail.com>
2023-03-14 07:26:53 +0100

    Spring Boot 3: upgrade chapter 04

--- chapter04/postcss.config.js
+++ chapter04/postcss.config.js
@@ -1,6 +1,17 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {}
-    }
+const postcssConfig = {
+    plugins: [require('autoprefixer'),
+        require('tailwindcss')],
+};
+
+// If we are in production mode, then add cssnano
+if (process.env.NODE_ENV === 'production') {
+    postcssConfig.plugins.push(
+        require('cssnano')({
+            // use the safe preset so that it doesn't
+            // mutate or remove code from our css
+            preset: 'default',
+        })
+    );
 }
+
+module.exports = postcssConfig;
---
+++ chapter04/src/main/java/com/tamingthymeleaf/application/HomeController.java
@@ -0,0 +1,15 @@
+package com.tamingthymeleaf.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String index(Model model) {
+        return "index";
+    }
+}
